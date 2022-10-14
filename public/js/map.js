d2e582feb4c281ce0a9a833027804c88abf80122
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-10-14 20:54:29 +0300

    getting the stores and shown on mapbox

---
+++ public/js/map.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXl1cHVjbWF6IiwiYSI6ImNsOGVoZzB2YzA0NDAzbnF4ZHloMGN1bG0ifQ.ywtVC0SKYt77XkH2ES80-g';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-71.157895, 42.707741],
+  zoom: 9,
+});
+
+// Fetch Stores from API
+async function getStores() {
+  const res = await fetch('/api/v1/stores');
+  const data = await res.json();
+
+  const stores = data.data.map((store) => {
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [store.location.coordinates[0], store.location.coordinates[1]],
+      },
+      properties: {
+        storeId: store.storeId,
+        icon: 'shop',
+      },
+    };
+  });
+
+  loadMap(stores);
+}
+
+// Load map with stores
+
+function loadMap(stores) {
+  map.on('load', function () {
+    map.addLayer({
+      id: 'points',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: stores,
+        },
+      },
+      layout: {
+        'icon-image': '{icon}-15',
+        'icon-size': 1.5,
+        'text-field': '{storeId}',
+        'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+        'text-offset': [0, 0.9],
+        'text-anchor': 'top',
+      },
+    });
+  });
+}
+
+getStores();

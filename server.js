6a3948e403ab02724380d68c268bfd2f3e03e82f
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-10-14 18:44:20 +0300

    route and db connection

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -15,9 +19,8 @@ app.use(express.json());
 // Enable CORS
 app.use(cors());
 
-app.get('/api/v1/stores', (req, res) => {
-  res.send('Hello World!');
-});
+// Routes
+app.use('/api/v1/stores', require('./routes/stores'));
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
